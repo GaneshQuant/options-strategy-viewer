7e584f56f05871fabcdb205e4a3250241f3c853c
GaneshQuant <ganesh.quant@gmail.com>
2025-01-12 19:16:51 +0530

    changing path1

--- strategy_viewer.py
+++ strategy_viewer.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Load the data files
 @st.cache
 def load_data():
-    strategy_levels_path = "/content/sample_data/strategy_levels_aligned.csv"
+    strategy_levels_path = "strategy_levels_aligned.csv"
     portfolio_decomposition_path = "/content/sample_data/portfolio_decomposition_aligned.csv"
 
     strategy_levels = pd.read_csv(strategy_levels_path)
